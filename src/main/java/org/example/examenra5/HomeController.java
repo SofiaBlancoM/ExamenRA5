73c41e561b49ffb684d053e671c4291ef3b601b6
Sofia Blanco <4870998@gmail.com>
2026-03-03 19:33:49 +0100

    feat: generate sample report with jasper viewer

--- src/main/java/org/example/examenra5/HomeController.java
+++ src/main/java/org/example/examenra5/HomeController.java
@@ -4,6 +4,11 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
+import net.sf.jasperreports.engine.*;
+import net.sf.jasperreports.view.JasperViewer;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 public class HomeController {
 
@@ -11,13 +16,25 @@ public class HomeController {
     public VBox root;
 
     @FXML
-    public void onCloseButtonAction(ActionEvent event) {
+    public void onOpenButtonAction(ActionEvent event) {
+
+        try(InputStream inputStream = MainApplication.class.getResourceAsStream("/playlist-report.jrxml")){
+
+            JasperReport jasperReport = JasperCompileManager.compileReport(inputStream);
+            JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, null, new JREmptyDataSource());
+            JasperViewer.viewReport(jasperPrint, false);
+
+        }
+        catch (JRException | IOException e){
+            System.out.println("Error al abrir el informe");
+            e.printStackTrace();
+        }
 
     }
 
     @FXML
-    public void onOpenButtonAction(ActionEvent event) {
-
+    public void onCloseButtonAction(ActionEvent event) {
+        System.exit(0);
     }
 
 }
